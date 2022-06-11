192a3435baf7f084c9fada64f30340bdcd784bf4
Sofiane Rehila <sofiane.rehila@gmail.com>
2022-06-11 06:47:31 +0200

    add cors

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require("dotenv").config();
 
 const express = require("express");
 const formidable = require("express-formidable");
+const cors = require("cors");
 
 const app = express();
 app.use(formidable());
+app.use(cors());
 
 const cloudinary = require("cloudinary").v2;
 
