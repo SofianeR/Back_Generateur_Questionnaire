77c71770479fc2cf8f11c008a5798db744232bdf
Sofiane Rehila <sofiane.rehila@gmail.com>
2022-06-11 06:40:04 +0200

    remove cors

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ require("dotenv").config();
 
 const express = require("express");
 const formidable = require("express-formidable");
-// const cors = require("cors");
 
 const app = express();
 app.use(formidable());
-
-// app.use(cors());
 
 const cloudinary = require("cloudinary").v2;
 
